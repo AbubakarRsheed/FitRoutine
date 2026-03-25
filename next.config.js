f67397fbe4850e5d8d505f89fcaca9f5274e4022
Abubakar Rasheed <abubakarrashed102@gmail.com>
2026-03-25 20:29:53 +0500

    added nextconsole error t

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Static HTML export enable karne ke liye ye line zaroori hai
+  // Remove output: 'export' for server-side rendering
   images: {
-    unoptimized: true, // Static export mein images ko optimize karne ke liye server chahiye hota hai, isliye ise true karna parta hai
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
       },
     ],
   },
